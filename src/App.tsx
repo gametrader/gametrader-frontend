ecfaac37d3bbb4629838a083c2a3b997718f0937
Krystian <krystian.trzesniak@gmail.com>
2022-03-31 03:11:01 +0200

    GAMETRADER-77: Added better footer using flexbox.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Router>
             <div className='gt-main'>
                 <Header/>
-                    <div className={"container"}>
+                    <div className={"gt-container"}>
                     <Routes>
                         <Route path='/' element={<HomePage />} />
                         <Route path='/register' element={<RegisterPage />} />
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,66 +7,33 @@ import logo from "../../assets/gametrader-logo-version-1.png";
 const Footer = () => {
     const navigate = useNavigate();
     return (
-        <footer className={"" + styles.gtFooter}>
-            <div className={"container " + styles.footerWrapper}>
-                <div className={"row " + styles.footer}>
-                    <div className={"col-xs col-sm-4 col-md-4 col-lg-4 col-xl-4 " + styles.leftSection}>
-                        <button type="button" className={styles.button + " btn gt-btn-primary"}
-                                onClick={() => navigate('/Help')}>
-                            Masz problem?
-                        </button>
-                    </div>
+        <footer className={styles.gtFooter}>
+            <div className={"gt-container " + styles.footer}>
 
-                    <div className={"col-xs col-sm-4 col-md-4 col-lg-4 col-xl-4 " + styles.centerSection}>
-                        <p className={styles.copyright}>GameTrader 2022</p>
-                    </div>
-                    <div className={"col-xs col-sm-4 col-md-4 col-lg-4 col-xl-4 " + styles.xd}>
-                        <div className={styles.rightSection}>
-                            <button type="button" className={styles.button + " btn gt-btn-primary"}
-                                    onClick={() => navigate('/Contact')}>
-                                Kontakt
-                            </button>
-                        </div>
-                        <div className={styles.rightSection}>
-                            <button type="button" className={styles.button + " btn gt-btn-primary"}
-                                    onClick={() => navigate('/AboutUs')}>
-                                O nas
-                            </button>
-                        </div>
-                    </div>
+                <div className={styles.leftSection}>
+                    <button type="button" className={styles.button + " btn gt-btn-primary"}
+                            onClick={() => navigate('/Help')}>
+                        Masz problem?
+                    </button>
                 </div>
+
+                <div className={styles.centerSection}>
+                    <p className={styles.copyright}>GameTrader 2022</p>
+                </div>
+
+                <div className={styles.rightSection}>
+                    <button type="button" className={styles.button + " btn gt-btn-primary"}
+                            onClick={() => navigate('/Contact')}>
+                        Kontakt
+                    </button>
+                    <button type="button" className={styles.button + " btn gt-btn-primary"}
+                            onClick={() => navigate('/AboutUs')}>
+                        O nas
+                    </button>
+                </div>
+
             </div>
         </footer>
-    // <nav className={'navbar sticky-top ' + styles.gtHeader}>
-    //     <div className={'container ' + styles.headerWrapper}>
-    //
-    //         <div className={"row " + styles.header}>
-    //
-    //             <div className={"col-xs col-sm-7 col-md-7 col-lg-6 col-xl-4 " + styles.leftSection}>
-    //                 <img className={"" + styles.logo} src={logo} onClick={ () => navigate('/')}  alt="App logo"/>
-    //                 <p className={"" + styles.motto + " gt-vt-font"}>SPRZEDAJ SPRZĘT<br></br>NIE KOLEGÓW</p>
-    //             </div>
-    //
-    //             <ul className={"col-xs col-sm-5 col-md-5 col-lg-4 col-xl-4 " + styles.rightSection}>
-    //                 {/* Add aditional buttons here if you want them to the left side */}
-    //                 {/*Być może tę ikonę powinniśmy umieścić w <button></button> bez styli*/}
-    //                 <li onClick={ () => navigate('/')}>
-    //                     <i className={"fa fa-bars"}/>
-    //                     <a>Dodaj</a>
-    //                 </li>
-    //                 <li onClick={ () => navigate('/')}>
-    //                     <i className={"fa fa-bars"}/>
-    //                     <a>Wiadomości</a>
-    //                 </li>
-    //                 <li onClick={ () => navigate('/')}>
-    //                     <i className={"fa fa-bars"}/>
-    //                     <a>Konto</a>
-    //                 </li>
-    //             </ul>
-    //
-    //         </div>
-    //     </div>
-    // </nav>
     );
 }
 
