a16e3e9dab684bd73e7db0c7448f219b3b9b16eb
Krystian <krystian.trzesniak@gmail.com>
2022-04-01 02:30:36 +0200

    GAMETRADER-64: Added static layout for login page.

--- src/components/login-page/LoginPage.tsx
+++ src/components/login-page/LoginPage.tsx
@@ -3,8 +3,29 @@ import styles from './LoginPage.module.scss'
 
 const LoginPage = () => {
     return (
-        <div className={styles.text}>
-            LoginPage
+        <div className={styles.gtLoginPage}>
+            <header>
+                <div className={"gt-container "}>
+                    <h1 className={"gt-vt-font "}>Logowanie</h1>
+                </div>
+            </header>
+            <div className={"gt-container " + styles.container}>
+                <form>
+                    <div className={styles.row}>
+                        <label htmlFor={"userName"}>Nazwa użytkownika</label>
+                        <input type="text" name="userName" />
+                    </div>
+
+                    <div className={styles.row}>
+                        <label htmlFor={"password"}>Hasło</label>
+                        <input type="text" name="password" />
+                    </div>
+
+                    <div className={styles.row}>
+                        <input className={"gt-vt-font btn gt-btn-secondary " + styles.loginBtn} type="submit" value="Zaloguj się" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
