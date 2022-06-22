ea05b74c374a6b58eebacdec6aa5ea79903fd053
Kris <87186748+Krystiano99@users.noreply.github.com>
2022-06-22 20:48:00 +0200

    Added view advertisement page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './components/home-page/HomePage';
 import RegisterPage from './components/register-page/RegisterPage';
 import LoginPage from './components/login-page/LoginPage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import AddAdvertisementPage from './components/add-advertisement-page/AddAdvertisementPage';
+import AdvertisementViewPage from './components/advertisement-view-page/AdvertisementViewPage';
 import SearchResultPage from './components/search-result-page/SearchResultPage';
 
 
@@ -14,15 +15,16 @@ const App = () => {
 	return (
 		<Router>
 			<div className='gt-main'>
-				<Header />
+				<Header/>
 				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/register' element={<RegisterPage />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/search-result' element={<SearchResultPage/>} />
-					<Route path='/add-advertisement' element={<AddAdvertisementPage />} />
+					<Route path='/' element={<HomePage/>}/>
+					<Route path='/register' element={<RegisterPage/>}/>
+					<Route path='/login' element={<LoginPage/>}/>
+					<Route path='/search-result' element={<SearchResultPage/>}/>
+					<Route path='/add-advertisement' element={<AddAdvertisementPage/>}/>
+					<Route path='/view-advertisement' element={<AdvertisementViewPage/>}/>
 				</Routes>
-				<Footer />
+				<Footer/>
 			</div>
 		</Router>
 	);
---
+++ src/components/advertisement-view-page/AdvertisementViewPage.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styles from './AdvertisementViewPage.module.scss';
+import example from '../../assets/debil.jpg';
+
+
+const AdvertisementViewPage = () => {
+	return (
+		<>
+			<div className={'gt-container '}>
+				<div className={'' + styles.header}>
+					<h2>TYTUL OGLOSZENIA</h2>
+				</div>
+			
+				<ul className={styles.imageContainer}>
+					<li className={'chevron'}>
+						<i className={'fa fa-chevron-left'}/>
+					</li>
+
+					<li style={{width: '100%'}}>
+						<img src={example}/>
+					</li>
+
+					<li className={'chevron'}>
+						<i className={'fa fa-chevron-right'}/>
+					</li>
+				</ul>
+
+
+				<div className={'' + styles.infoContainer}>
+					<p className={'' + styles.price}>
+						22zł
+					</p>
+					<div className={'' + styles.description}>
+						<h3>OPIS</h3>
+						<p>
+							Witam, na sprzedaż mam 2 książki, są to:
+
+							1.Wędrówki po Lubelszczyźnie Leona Wyszczółkowskiego. Katalog wystawy z okazji 100 lecia
+							Muzeum Lubelskiego oraz 70 rocznicy śmierci artysty, książka z dziełami.
+
+							2. LUBLIN. Książka z 1956r z powojennymi zdjęciami stolicy Lubelszczyzny. W kolorze czarno
+							białym, z uwagi na wiek trochę pożółkła.
+
+							Książki w stanie bardzo dobrym, szczególnie album Wyszczółkowskiego.
+
+							Zapraszam!
+						</p>
+					</div>
+				</div>
+
+
+			</div>
+		</>
+	);
+};
+
+
+export default AdvertisementViewPage;
