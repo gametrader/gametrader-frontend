43175690db55823eb6cd9812fbfd047e3e31ea97
Kris <87186748+Krystiano99@users.noreply.github.com>
2022-06-22 22:59:07 +0200

    Added routing to viewAdvertisement page

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './HomePage.module.scss';
 import Advertisement from './../advertisement/Advertisement';
-import { CategoryModel } from '../../models/CategoryModel';
+import {CategoryModel} from '../../models/CategoryModel';
 import Category from '../category/Category';
-import { AdvertisementModel } from '../../models/AdvertisementModel';
+import {AdvertisementModel} from '../../models/AdvertisementModel';
 import SearchBar from '../search-bar/SearchBar';
 import advertisementCategoriesService from '../../services/AdvertisementCategoriesService';
 import advertisementService from '../../services/AdvertisementService';
+import {useNavigate} from 'react-router-dom';
 
 const HomePage = () => {
 	const [allCategories, setAllCategories] = useState<CategoryModel[]>([]);
@@ -21,6 +22,8 @@ const HomePage = () => {
 	const [selectedAdvertisements, setSelectedAdvertisements] = useState<AdvertisementModel[]>([]);
 	const [selectedAdvertisementsStartIndex, setSelectedAdvertisementsStartIndex] = useState<number>(0);
 	const [selectedAdvertisementsEndIndex, setSelectedAdvertisementsEndIndex] = useState<number>(6);
+
+	const navigate = useNavigate();
 
 	const setCategoriesSlider = () => {
 		setCategories(allCategories.slice(categoriesStartIndex, categoriesEndIndex));
@@ -83,6 +86,11 @@ const HomePage = () => {
 		});
 	};
 
+	const routeToAdvertisementView = () => {
+		const path = '/view-advertisement';
+		navigate(path);
+	};
+
 	useEffect(() => {
 		// Add initializing logic here
 		getAllCategoriesList();
@@ -100,20 +108,20 @@ const HomePage = () => {
 
 	return (
 		<>
-			<SearchBar />
+			<SearchBar/>
 			<div className={'gt-container '}>
 				<div className={styles.AdvertisementsSelectedForUser}>
 					<h2>Wybrane dla Ciebie</h2>
 					<ul className={' ' + styles.AdvertisementsSelectedContainer}>
-						<li onClick={ () => moveSelectedAdvertisementsSlider('left')} className={styles.chevron}>
+						<li onClick={() => moveSelectedAdvertisementsSlider('left')} className={styles.chevron}>
 							<i className={'fa fa-chevron-left'}/>
 						</li>
-						{ selectedAdvertisements.map((advertisement, index) => (
-							<li key={index}>
-								<Advertisement advertisement={advertisement} />
+						{selectedAdvertisements.map((advertisement, index) => (
+							<li key={index} onClick={routeToAdvertisementView}>
+								<Advertisement advertisement={advertisement}/>
 							</li>
 						))}
-						<li onClick={ () => moveSelectedAdvertisementsSlider('right')} className={styles.chevron}>
+						<li onClick={() => moveSelectedAdvertisementsSlider('right')} className={styles.chevron}>
 							<i className={'fa fa-chevron-right'}/>
 						</li>
 					</ul>
@@ -122,15 +130,15 @@ const HomePage = () => {
 				<div className={styles.allCategories}>
 					<h2>Wszystkie kategorie</h2>
 					<ul className={styles.allCategoriesContainer}>
-						<li onClick={ () => moveCategoriesSlider('left')} className={styles.chevron}>
+						<li onClick={() => moveCategoriesSlider('left')} className={styles.chevron}>
 							<i className={'fa fa-chevron-left'}/>
 						</li>
-						{ categories.map((category, index) => (
+						{categories.map((category, index) => (
 							<li key={index} style={{width: '100%'}}>
 								<Category category={category}/>
 							</li>
 						))}
-						<li onClick={ () => moveCategoriesSlider('right')} className={styles.chevron}>
+						<li onClick={() => moveCategoriesSlider('right')} className={styles.chevron}>
 							<i className={'fa fa-chevron-right'}/>
 						</li>
 					</ul>
@@ -139,9 +147,9 @@ const HomePage = () => {
 				<div className={styles.lastAddedAdvertisements}>
 					<h2>Ostatnio dodane</h2>
 					<ul className={' ' + styles.lastAdvertisementsContainer}>
-						{ allRecentlyAddedAdvertisements.map((advertisement, index) => (
+						{allRecentlyAddedAdvertisements.map((advertisement, index) => (
 							<li key={index}>
-								<Advertisement advertisement={advertisement} />
+								<Advertisement advertisement={advertisement}/>
 							</li>
 						))}
 					</ul>
